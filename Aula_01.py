a5a9586fc09973d18b008b54309909459e237301
agbrito <angelobrito2315@gmail.com>
2026-05-04 22:05:55 -0300

    Aula 01 concluida

--- Aula_01.py
+++ Aula_01.py
@@ -1,1 +1,24 @@
-print("Hello pimbas")
+
+    # Desafio 01
+
+print("Angelo")
+print("Acaraú")
+print("18 anos")
+
+    #Desafio 02
+print("Angelo\n Acaraú\n 18 anos")
+
+    #desafio 03
+print(sep=" ")
+print("Angelo", "Acaraú", "18 anos")
+
+    #Desafio 04
+print(sep=" " )
+
+print("Angelo", "Acaraú", "18 anos", sep=" - ")
+
+    #Desafio 05
+print(sep=" " )
+
+print("Angelo\t Acaraú\t 18 anos")
+
