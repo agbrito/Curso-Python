ff5bb711d50c46b7cdfc7ffb07c541c8286a1885
agbrito <angelobrito2315@gmail.com>
2026-05-05 21:49:10 -0300

    aula 02 completa

---
+++ Aula_02.py
@@ -0,0 +1,12 @@
+# nomeCompleto = "Angelo Gabriel Pinto Brito"
+idade = 18
+altura = 1.74
+gosta_de_python = True 
+
+print(type(nomeCompleto))
+print(type(idade))
+print(type(altura))
+print(type(gosta_de_python))
+
+print(nomeCompleto, "\t", idade, "\n", altura, "\n", gosta_de_python, sep=" - ")
+
